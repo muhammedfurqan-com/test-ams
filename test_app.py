a78d1795795fd17202927c1a38fde13663f12490
muhammedfurqan-com <muhammedfurqan@gmail.com>
2025-09-22 15:20:20 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -63,14 +63,14 @@ st.title("Path Profile Checker")
 #tx = st.text_input("TX coordinates (lat,lon)", "33.6844,73.0479")
 #rx = st.text_input("RX coordinates (lat,lon)", "32.0836,72.6711")
 
-if st.button("Check Path Profile"):
-    path = f"{tx}|{rx}"
-    url = f"https://maps.googleapis.com/maps/api/elevation/json?path={path}&samples=50&key={API_KEY}"
-    response = requests.get(url).json()
-    st.write(response)
+#if st.button("Check Path Profile"):
+ #   path = f"{tx}|{rx}"
+  #  url = f"https://maps.googleapis.com/maps/api/elevation/json?path={path}&samples=50&key={API_KEY}"
+   # response = requests.get(url).json()
+    #st.write(response)
 
 
-    profile = [p["elevation"] for p in response["results"]]
+   # profile = [p["elevation"] for p in response["results"]]
 
     # Create figure
     fig, ax = plt.subplots(figsize=(8,4))
