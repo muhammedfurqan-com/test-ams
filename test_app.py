ed274e6d6163f41936ed03318fdc7a031f78cfbe
muhammedfurqan-com <muhammedfurqan@gmail.com>
2025-09-22 15:23:17 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -64,16 +64,17 @@ st.title("Path Profile Checker")
 #rx = st.text_input("RX coordinates (lat,lon)", "32.0836,72.6711")
 
 #if st.button("Check Path Profile"):
- #   path = f"{tx}|{rx}"
-  #  url = f"https://maps.googleapis.com/maps/api/elevation/json?path={path}&samples=50&key={API_KEY}"
-   # response = requests.get(url).json()
+path = f"{tx}|{rx}"
+url = f"https://maps.googleapis.com/maps/api/elevation/json?path={path}&samples=50&key={API_KEY}"
+response = requests.get(url).json()
     #st.write(response)
+profile = [p["elevation"] for p in response["results"]]
 
 
    
 
     # Create figure
-    profile = [p["elevation"] for p in response["results"]]
+    
     fig, ax = plt.subplots(figsize=(8,4))
     ax.plot(profile, color="blue", linewidth=2)   # Blue line
     ax.fill_between(range(len(profile)), profile, color="skyblue", alpha=0.3)  # Fill under curve
