89d10ab8dd5eea31c3357c9656ffdff0062927c6
muhammedfurqan-com <muhammedfurqan@gmail.com>
2025-09-22 15:26:49 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -58,26 +58,26 @@ if st.button("Generate Path Profile"):
         st.error(f"Error fetching data: {response.get('error_message', response.get('status'))}")
 
 
-st.title("Path Profile Checker")
+#st.title("Path Profile Checker")
 
 #tx = st.text_input("TX coordinates (lat,lon)", "33.6844,73.0479")
 #rx = st.text_input("RX coordinates (lat,lon)", "32.0836,72.6711")
 
 #if st.button("Check Path Profile"):
-path = f"{tx}|{rx}"
-url = f"https://maps.googleapis.com/maps/api/elevation/json?path={path}&samples=50&key={API_KEY}"
-response = requests.get(url).json()
+#path = f"{tx}|{rx}"
+#url = f"https://maps.googleapis.com/maps/api/elevation/json?path={path}&samples=50&key={API_KEY}"
+#response = requests.get(url).json()
     #st.write(response)
-profile = [p["elevation"] for p in response["results"]]
+#profile = [p["elevation"] for p in response["results"]]
    
     # Create figure
     
-fig, ax = plt.subplots(figsize=(8,4))
-ax.plot(profile, color="blue", linewidth=2)   # Blue line
-ax.fill_between(range(len(profile)), profile, color="skyblue", alpha=0.3)  # Fill under curve
-ax.set_title("Elevation Profile", fontsize=14)
-ax.set_xlabel("Sample Point")
-ax.set_ylabel("Elevation (m AMSL)")
-ax.grid(True, linestyle="--", alpha=0.5)
+#fig, ax = plt.subplots(figsize=(8,4))
+#ax.plot(profile, color="blue", linewidth=2)   # Blue line
+#ax.fill_between(range(len(profile)), profile, color="skyblue", alpha=0.3)  # Fill under curve
+#ax.set_title("Elevation Profile", fontsize=14)
+#ax.set_xlabel("Sample Point")
+#ax.set_ylabel("Elevation (m AMSL)")
+#ax.grid(True, linestyle="--", alpha=0.5)
 
-st.pyplot(fig)
+#st.pyplot(fig)
